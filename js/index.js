dd490e37ae0758faea5215353ececa90c5ddc9cd
Ondrej Zeman <ozeman77@gmail.com>
2018-02-27 00:35:10 +0100

    background image

--- js/index.js
+++ js/index.js
@@ -2,14 +2,40 @@ require('../css/index.styl')
 
 function changeMenuFocus(e) {
 
+    e.preventDefault()
     let lastbtn = document.querySelector('.active')
     if (lastbtn) {
         lastbtn.classList.remove('active');
     }
-    e.target.className += "active"
-
+    e.target.className += "active";
+    smoothScroll(document.querySelector(e.target.hash));
+    window.location.href = e.target.hash;
 }
 (() => {
     document.querySelectorAll(".navbtn")
         .forEach(b => b.addEventListener("click", changeMenuFocus));
 })()
+
+
+window.smoothScroll = function (target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function (c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function () { scroll(c, a, b, i); }, 10);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
