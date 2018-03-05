5ae10b4a71b0bbfca671362ff610387a9999ff0d
Ondrej Zeman <ozeman77@gmail.com>
2018-03-05 15:39:14 +0100

    stylus rework

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 require('../css/index.styl')
 import jump from 'jump.js'
 
+// const sections = document.querySelectorAll('.navbtn');
+// var activeSection = window.location.href;
+
 function changeMenuFocus(e) {
 
     e.preventDefault()
@@ -9,13 +12,39 @@ function changeMenuFocus(e) {
         lastbtn.classList.remove('active');
     }
     e.target.className += "active";
-    jump(e.target.hash,{duration: 250})
+    jump(e.target.hash, {
+        duration: 250,
+        offset: -50
+    })
     window.location.href = e.target.hash;
 }
 
+function changeActiveSection(section) {
+    let lastbtn = document.querySelector('.active')
+    if (lastbtn&& `#${lastbtn.id}` === section) return;
+    if (lastbtn) {
+        lastbtn.classList.remove('active');
+    }
+
+    let btn = document.querySelector(section)
+    console.log(btn,section)
+    btn.className += "active";
+    history.pushState(null, null, `#${btn.href.split('#')[1]}`);
+
+}
+function getIdFromHref(){
+
+    let tmp =window.location.href.split('#');
+    if(tmp.length===1){
+        return "#btn-home"
+    }
+    return `#btn-${tmp[1].toLowerCase()}`;
+}
 
 
 (() => {
+    changeActiveSection(getIdFromHref());
+
     document.querySelectorAll(".navbtn")
         .forEach(b => b.addEventListener("click", changeMenuFocus));
 
@@ -24,6 +53,33 @@ function changeMenuFocus(e) {
             document.body.style.opacity = "100";
         }, 1);
     };
+    window.onscroll = () => {
+
+        if (checkFullyVisible('#About')) {
+            changeActiveSection("#btn-about")
+
+        } else if (checkFullyVisible('#Contact')) {
+            changeActiveSection("#btn-contact")
+
+        } else if (checkFullyVisible('#Home')) {
+            changeActiveSection("#btn-home")
+
+        } else if (checkFullyVisible('#MyStuff')) {
+            changeActiveSection("#btn-mystuff")
+        }
+        //doesnt work mystuff
+    };
 
 })()
+function checkFullyVisible(elm) {
 
+    var rect = document.querySelector(elm).getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+
+    // if(elm.id==="Home")console.log(rect, elm,viewHeight)
+    return (rect.top >= 50) && (rect.top <= viewHeight - rect.height)
+        || (rect.height >= viewHeight
+            && (rect.top <= 50 && rect.top >= viewHeight - rect.height))//this is bad
+
+    // return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
